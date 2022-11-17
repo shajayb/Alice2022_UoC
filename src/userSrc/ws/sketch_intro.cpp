de752b59443bca674df566a635edd7a6d1c10e53
shajay <shajay.bhooshan@gmail.com>
2022-11-17 12:16:35 +0000

    session2

--- src/userSrc/ws/sketch_intro.cpp
+++ src/userSrc/ws/sketch_intro.cpp
@@ -35,16 +35,53 @@ IO fileImporter;
 // vectors and transformations.
 // transfromation = translation, rotation, scale
 
+// global scope of a variable --> meaning the names and the contents of the variable are visible everywhere in the code.
 zPoint a, b, o;
 zVector center;
+zTransform tmat;
+zPointArray myPointArray;
+
+
 
 void setup() // EVENT 
 {
+
+	// local scope --> meaning the name and contents are only visible within setup ;
 	a = zPoint(-5, 5, 0);
 	b = zPoint(5, 5, 0);
 	o = zPoint(0, 0, 0);
 
+	// arrays ;
+	myPointArray.push_back(a); // adding a point into myPointArray ;
+	myPointArray.push_back(b);
+	myPointArray.push_back(o);
+
+	// construct the **transformation matrix** to represent the rotated and translated **frame**
+
 	center = zVector(15, 5, 0);
+
+	zVector xAxis, yAxis, zAxis;
+
+	//
+	xAxis = zVector(1, -5, 0); // xAxis of the frame
+	zAxis = zVector(0, 0, 1); // zAxis of the frame
+	yAxis = xAxis ^ zAxis; // use cross product to calculate the yAxis of the frame
+	
+	// normalise the axes;
+	xAxis.normalize();
+	yAxis.normalize();
+	zAxis.normalize();
+
+	// insert the axes into the columns of hte transformation matrix ;
+	tmat.col(0) << xAxis.x, xAxis.y, xAxis.z, 1;
+	tmat.col(1) << yAxis.x, yAxis.y, yAxis.z, 1;
+	tmat.col(2) << zAxis.x, zAxis.y, zAxis.z, 1;
+	tmat.col(3) << center.x, center.y, center.z, 1;
+
+	//a = a * tmat; // multiply orignal point with the transformation matrix to get the new point
+	//b = b * tmat;
+	//o = o * tmat;
+	// use the transformation matrix, to transform the original points to their new location i.e to the new frame.
 
 	
 }
@@ -55,12 +92,7 @@ void setup() // EVENT
 void update(int value)
 {
 
-	// LHS = RHS
-	center.x = center.x + 0.001;
-
-	a = a + center;
-	b = b + center;
-	o = o + center;
+	
 }
 	
 // happens 100 times a second
@@ -70,12 +102,11 @@ void draw()
 	drawGrid(50);
 
 
-	Dsp.drawPoint(a, zColor(1, 0, 0, 1), 8);
-	Dsp.drawPoint(b, zColor(0, 0, 1, 1), 8);
 
-	Dsp.drawLine(a, b);
-	Dsp.drawLine(a, o);
-	Dsp.drawLine(b, o);
+	for (int i = 0; i < 3; i++)
+	{
+		Dsp.drawPoint(myPointArray[i], zColor(1, 0, 0, 1), 8);
+	}
 	
 }
 
@@ -83,6 +114,11 @@ void draw()
 void keyPress(unsigned char k, int xm, int ym)
 {
 	
+	for (int i = 0; i < myPointArray.size(); i++)
+	{
+		myPointArray[i] = myPointArray[i] * tmat;
+	}
+
 }
 
 void mousePress(int b, int state, int x, int y)
